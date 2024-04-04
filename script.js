97624655f5684c4d6c6618247c466526eb0f85db
Kenneth Tran <trankenneth133@gmail.com>
2024-04-03 23:05:37 -0700

    Added add,subtract,multiply, and divide functions in script.js

---
+++ script.js
@@ -0,0 +1,16 @@
+function add(firstInput,secondInput){
+    return firstInput + secondInput;
+}
+function subtract(firstInput,secondInput){
+    return firstInput - secondInput;
+}
+function multiply(firstInput,secondInput){
+    return firstInput * secondInput;
+}
+function divide(firstInput,secondInput){
+    return firstInput / secondInput;
+}
+console.log(add(1,2));
+console.log(subtract(2,1));
+console.log(multiply(3,4));
+console.log(divide(4,2));
