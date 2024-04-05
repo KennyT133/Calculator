c3c912584ddbf05fdc7d5b00ef7762d772c5c835
Kenneth Tran <trankenneth133@gmail.com>
2024-04-05 14:46:53 -0700

    Added equal function, operator function, and clear function

--- script.js
+++ script.js
@@ -1,31 +1,30 @@
 var firstNum,secondNum,operator,displayNumber = 0;
+var firstNum, secondNum, operator = null;
 const displayContainer = document.getElementById("display");
 const operands = document.getElementsByClassName("operand");
-
-function add(firstInput,secondInput){
-    return firstInput + secondInput;
-}
-function subtract(firstInput,secondInput){
-    return firstInput - secondInput;
-}
-function multiply(firstInput,secondInput){
-    return firstInput * secondInput;
-}
-function divide(firstInput,secondInput){
-    return firstInput / secondInput;
-}
+const operators = document.getElementsByClassName("operator");
+const equals = document.getElementsByClassName("equal");
+const clears = document.getElementsByClassName("clear");
 function operate(firstNum,secondNum,operator){
     if(operator == "+"){
-        add(firstNum,secondNum);
+        firstNum = Number(firstNum);
+        secondNum = Number(secondNum);
+        return firstNum + secondNum;
     }
     if(operator == "-"){
-        subtract(firstNum,secondNum);
+        firstNum = Number(firstNum);
+        secondNum = Number(secondNum);
+        return firstNum - secondNum;
     }
     if(operator == "/"){
-        divide(firstNum,secondNum);
+        firstNum = Number(firstNum);
+        secondNum = Number(secondNum);
+        return firstNum/secondNum;
     }
     if(operator == "*"){
-        multiply(firstNum,secondNum);
+        firstNum = Number(firstNum);
+        secondNum = Number(secondNum);
+        return firstNum * secondNum;
     }
 }
 
@@ -33,18 +32,63 @@ function displayOperand(e){
     if(firstNum == null){
         if(displayNumber == 0){
             displayNumber = e.target.value;
-            console.log(displayNumber);
         }
         else{
             displayNumber += e.target.value;
-            console.log(displayNumber);
+        }
+    }
+    else {
+        if(displayNumber == firstNum) {
+            displayNumber = e.target.value;
+        } else {
+            displayNumber+= e.target.value;
         }
     }
     updateDisplay();
 }
-for(i = 0; i < operands.length;i++){
+function equalOperate(e){
+   secondNum = displayNumber;
+   let result = operate(firstNum,secondNum,operator);
+   displayNumber = result;
+   updateDisplay();
+   firstNum = null;
+   secondNum = null;
+   operator = null;
+}
+function clearOperate(e){
+   firstNum = null;
+   secondNum = null;
+   operator = null;
+   displayNumber = 0;
+   updateDisplay();
+}
+function computeOperater(e){
+    if(firstNum == null){
+        firstNum = displayNumber;
+    }
+    else if(secondNum == null){
+        secondNum = displayNumber;
+    }
+    if(firstNum != null && secondNum != null && operator != null){
+        
+        let result = operate(firstNum,secondNum,operator);
+        displayNumber = result;
+        updateDisplay();
+        firstNum = result;
+        secondNum = null;
+
+    }
+    operator = e.target.value;
+   
+}
+for(let i = 0; i < operands.length;i++){
     operands[i].addEventListener("click",displayOperand);
 }
+for (let i = 0; i < operators.length;i++){
+    operators[i].addEventListener("click",computeOperater);
+}
+equals[0].addEventListener("click",equalOperate);
+clears[0].addEventListener("click",clearOperate);
 function updateDisplay(){
     displayContainer.innerText = displayNumber;
 }
