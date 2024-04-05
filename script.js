e60a76dd935986b298b81fe230c668946d2f38b8
Kenneth Tran <trankenneth133@gmail.com>
2024-04-04 20:12:19 -0700

    Cleaned up script.js and fixed logic for displaying numbers

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-var firstNum,secondNum,operator,displayNumber;
+var firstNum,secondNum,operator,displayNumber = 0;
 const displayContainer = document.getElementById("display");
 const operands = document.getElementsByClassName("operand");
-let displayDiv = document.createElement("div");
+
 function add(firstInput,secondInput){
     return firstInput + secondInput;
 }
@@ -28,20 +28,25 @@ function operate(firstNum,secondNum,operator){
         multiply(firstNum,secondNum);
     }
 }
-/*
-document.getElementById("one").addEventListener("click",function(){
-      displayNumber = 1;
-      let div = document.createElement("div");
-      div.textContent = displayNumber;
-      displayContainer.appendChild(div);
-});
-*/
+
 function displayOperand(e){
-    var displayNumber = e.target.value;
-    displayDiv.textContent = displayNumber;
-    console.log(displayNumber);
+    if(firstNum == null){
+        if(displayNumber == 0){
+            displayNumber = e.target.value;
+            console.log(displayNumber);
+        }
+        else{
+            displayNumber += e.target.value;
+            console.log(displayNumber);
+        }
+    }
+    updateDisplay();
 }
 for(i = 0; i < operands.length;i++){
     operands[i].addEventListener("click",displayOperand);
 }
-displayContainer.appendChild(displayDiv);
+function updateDisplay(){
+    displayContainer.innerText = displayNumber;
+}
+
+updateDisplay();
