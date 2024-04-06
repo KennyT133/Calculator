c0171fa9bc2fb3feafbc9a2492252aa52433a8f8
Kenneth Tran <trankenneth133@gmail.com>
2024-04-05 19:43:11 -0700

    Added logic to handle dividing by 0 and not to break calculator after dividing by 0

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
-var firstNum,secondNum,operator,displayNumber = 0;
+var displayNumber = 0;
 var firstNum, secondNum, operator = null;
 const displayContainer = document.getElementById("display");
 const operands = document.getElementsByClassName("operand");
 const operators = document.getElementsByClassName("operator");
 const equals = document.getElementsByClassName("equal");
 const clears = document.getElementsByClassName("clear");
+function updateDisplay(){
+    displayContainer.innerText = displayNumber;
+}
 function operate(firstNum,secondNum,operator){
     if(operator == "+"){
         firstNum = Number(firstNum);
@@ -19,7 +22,12 @@ function operate(firstNum,secondNum,operator){
     if(operator == "/"){
         firstNum = Number(firstNum);
         secondNum = Number(secondNum);
-        return firstNum/secondNum;
+        if(secondNum == 0){
+            return ("XD");
+        }
+        var value = firstNum/secondNum;
+        value = Math.round(value*100)/100;
+        return value;
     }
     if(operator == "*"){
         firstNum = Number(firstNum);
@@ -29,6 +37,10 @@ function operate(firstNum,secondNum,operator){
 }
 
 function displayOperand(e){
+    if(displayNumber == "XD" && firstNum == null & secondNum == null && operator == null){
+        displayNumber =0;
+        
+    }
     if(firstNum == null){
         if(displayNumber == 0){
             displayNumber = e.target.value;
@@ -56,7 +68,8 @@ function equalOperate(e){
    operator = null;
 }
 function clearOperate(e){
-   firstNum = null;
+   firstNum = null;displayNumber = "XD";
+   updateDisplay;
    secondNum = null;
    operator = null;
    displayNumber = 0;
@@ -70,16 +83,14 @@ function computeOperater(e){
         secondNum = displayNumber;
     }
     if(firstNum != null && secondNum != null && operator != null){
-        
         let result = operate(firstNum,secondNum,operator);
         displayNumber = result;
         updateDisplay();
         firstNum = result;
         secondNum = null;
-
     }
     operator = e.target.value;
-   
+    
 }
 for(let i = 0; i < operands.length;i++){
     operands[i].addEventListener("click",displayOperand);
@@ -89,8 +100,6 @@ for (let i = 0; i < operators.length;i++){
 }
 equals[0].addEventListener("click",equalOperate);
 clears[0].addEventListener("click",clearOperate);
-function updateDisplay(){
-    displayContainer.innerText = displayNumber;
-}
+
 
 updateDisplay();
